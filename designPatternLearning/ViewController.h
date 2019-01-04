101a5f130b3e7d6e883dcda16126f57496dfb718
chenxiaosong <smallhorse87@163.com>
2019-01-04 11:49:54 +0800

    Initial Commit

---
+++ designPatternLearning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  designPatternLearning
+//
+//  Created by chenxiaosong on 2019/1/4.
+//  Copyright © 2019年 chenxiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ designPatternLearning/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  designPatternLearning
+//
+//  Created by chenxiaosong on 2019/1/4.
+//  Copyright © 2019年 chenxiaosong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
