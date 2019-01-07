08b7dc3ab26d54b07515ac9dc5cba6c2ab735c7f
chenxiaosong <smallhorse87@163.com>
2019-01-07 09:56:12 +0800

    定好框架

--- designPatternLearning/ViewController.m
+++ designPatternLearning/ViewController.m
@@ -11,7 +11,7 @@
 #import "SimpleFactory.h"
 
 #import "ExportTxtFileOperate.h"
-//#import "TxtBuilder.h"
+#import "TxtBuilder.h"
 #import "Director.h"
 
 @interface ViewController ()
@@ -24,26 +24,61 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-//---------------
-//模式 之 创建类
-//---------------
+//-----------------
+//模式 之 创建类 共6个
+//-----------------
+    [self creationPatterns];
+    
 
+//-----------------
+//模式 之 结构类 共7个
+//-----------------
+    [self structurePatterns];
+
+//-----------------
+//模式 之 行为类 共11个
+//-----------------
+    [self behaviorPatterns];
+
+}
+
+- (void)structurePatterns
+{
+    
+}
+
+- (void)behaviorPatterns
+{
+
+}
+
+- (void)creationPatterns
+{
+    //原型模式
+    //太简单了，略
+    
+    //单例模式
+    //AppDelegate就是单例
+    
     //简单工厂：提供一个创建对象实例的功能，而无须关心其具体实现。被创建实例的类型可以是接口、抽象类，也可以是具体的类。
+    //核心：
     //优点：无需了解构建过程，集中管理。
     //缺点：需要传入参数，获取对应对象。
     id<Api> api = [SimpleFactory createApi:1];
     [api printFixedString];
-
+    
     //工厂方法：定义一个用于创建对象的接口，让子类决定实例化哪一个类，Factory Method使一个类的实例化延迟到其子类。
     //解释：‘子类’指的是抽象工厂的子类。
+    //核心：
     //优点：连什么产品都不需要知道；一个工厂负责一个产品的生产。
     ExportOperate *operate = [[ExportTxtFileOperate alloc] init];
     [operate export:@"测试数据"];
     
-    
+    //抽象工厂模式：
     
     
     //生成器：将一个复杂对象的构建与它的表示分离，使得同样的构建过程可以创建不同的表示。
+    //核心：
     ExportHeaderModel *ehm = [[ExportHeaderModel alloc] init];
     ehm.depId = @"一分公司";
     ehm.exportDate = @"2018-05-18";
@@ -68,14 +103,14 @@
     ExportFooterModel *efm = [[ExportFooterModel alloc] init];
     efm.exportUser = @"张三";
     
-//    TxtBuilder *txtBuilder = [[TxtBuilder alloc] init];
-//    
-//    Director *director = [[Director alloc] init];
-//    director.builder = txtBuilder;
-//    
-//    [director construct:ehm data:mapData footer:efm];
-//    
-//    NSLog(@"\n输出文本文件的内容：\n%@",[txtBuilder getResult]);
+    TxtBuilder *txtBuilder = [[TxtBuilder alloc] init];
+    
+    Director *director = [[Director alloc] init];
+    director.builder = txtBuilder;
+    
+    [director construct:ehm data:mapData footer:efm];
+    
+    NSLog(@"\n输出文本文件的内容：\n%@",[txtBuilder getResult]);
 }
 
 @end
